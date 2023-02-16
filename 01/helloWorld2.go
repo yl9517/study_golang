b2941120054d6b107459dabcb7e2ef0bf6f3c5b0
summer <80736033+yl9517@users.noreply.github.com>
2023-02-16 10:02:02 +0900

    println은 값의 출력만 처리하며, fmt 패키지는 출력 문자열의 형식과 구조를 제어할 수 있음

--- 01/helloWorld2.go
+++ 01/helloWorld2.go
@@ -2,7 +2,10 @@ package main
 
 
 func main() {
+    println("Hello, Summer!")
 	name := "John"
 	age := 30
     println("My name is %s and I'm %d years old.\n", name, age)
+    //My name is %s and I'm %d years old.
+    //John 30
 }
